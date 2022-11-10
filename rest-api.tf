f89425724f1c2fd45604dc342713aa2846aa2adb
Yusuf Bolaji <61923150+BigB97@users.noreply.github.com>
2022-11-10 11:46:13 +0100

    Update rest-api.tf

--- rest-api.tf
+++ rest-api.tf
@@ -23,35 +23,41 @@ resource "aws_api_gateway_method" "name" {
   rest_api_id   = aws_api_gateway_rest_api.name.id
   resource_id   = aws_api_gateway_resource.name.id
   http_method   = "GET"
-  authorization = AWS_IAM
-  integration {
-    type                    = AWS
-    integration_http_method = "POST"
-    uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.name.arn}/invocations"
-    integration_responses {
-      status_code = 200
-    }
-    request_templates = {
-      "application/json" = <<EOF
-      {
-        "username":"$input.params('username')",
-        "password":"$util.escapeJavaScript($input.params('password')).replaceAll("\\'","'")",
-        "serverId":"$input.params('serverId')",
-      }
-      EOF
-    }
-    request_parameters = {
-      method.request.header.password = false
-    }
-    method_responses {
-      status_code = 200
-      response_models = {
-        "application/json" = aws_api_gateway_model.name.name
-      }
-    }
+  authorization = "AWS_IAM"
+}
+
+# method response
+resource "aws_api_gateway_method_response" "name" {
+  rest_api_id = aws_api_gateway_rest_api.name.id
+  resource_id = aws_api_gateway_resource.name.id
+  http_method = aws_api_gateway_method.name.http_method
+  status_code = 200
+  response_models = {
+    "application/json" = aws_api_gateway_model.name.name
   }
 }
 
+resource "aws_api_gateway_integration" "name" {
+  rest_api_id             = aws_api_gateway_rest_api.name.id
+  resource_id             = aws_api_gateway_resource.name.id
+  http_method             = aws_api_gateway_method.name.http_method
+  type                    = "AWS"
+  integration_http_method = "POST"
+  uri                     = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.name.arn}/invocations"
+  request_templates = {
+    "application/json" = <<EOF
+    {
+      "username":"$input.params('username')",
+      "password":"$util.escapeJavaScript($input.params('password')).replaceAll("\\'","'")",
+      "serverId":"$input.params('serverId')",
+    }
+    EOF
+  }
+  request_parameters = {
+    "method.request.header.password" = false
+  }
+
+}
 
 resource "aws_api_gateway_model" "name" {
   rest_api_id  = aws_api_gateway_rest_api.name.id
